76bc1f855aee8b0ab1e59b8d31a3804768567cda
Peregrino Mendoza <pymendoza@gmail.com>
2021-10-06 17:54:38 +0800

    Initial commit

---
+++ OpenXML.PPT/Program.cs
@@ -0,0 +1,61 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using System.IO;
+
+namespace OpenXML.PPT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var replaceName = "Leader X";
+
+            var path = @"E:\Downloads\Upwork part I.pptx";
+            
+            using (PresentationDocument wd = PresentationDocument.Open(path, true))
+            {
+                var presentationPart = wd.PresentationPart.GetPartById("rId2");
+
+                foreach (var part in presentationPart.Parts)
+                {
+                    if (part.RelationshipId == "rId2")
+                    {
+                        foreach (var innerPart in part.OpenXmlPart.Parts)
+                        {
+                            if (part.RelationshipId == "rId2") 
+                            {
+                                OpenXmlElement replace = null;
+                                OpenXmlElement old = null;
+
+                                var slide = ((SlidePart)innerPart.OpenXmlPart).Slide;
+
+                                foreach (var childElemet in slide.ChildElements)
+                                {
+                                    if (childElemet.LocalName == "cSld")
+                                    {
+                                        old = childElemet;
+                                        replace = childElemet.Clone() as OpenXmlElement;
+                                        break;
+                                    }
+                                }
+
+                                replace.InnerXml = replace.InnerXml.Replace("{{name}}", replaceName);
+                                
+                                slide.ReplaceChild(replace, old);
+
+                                var dir = Path.GetDirectoryName(path);
+                                var filename = Path.GetFileNameWithoutExtension(path);
+                                var ext = Path.GetExtension(path);
+
+                                var newFilename = Path.Combine(dir, $"{filename}-copy{ext}");
+                                
+                                wd.SaveAs(newFilename);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
